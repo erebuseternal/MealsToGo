5d54197010dfd9aa0b8c6dd36b863052c7446be3
erebuseternal <marcelsanders96@gmail.com>
2021-12-29 10:35:01 -0500

    navigation

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
+/* eslint-disable react/no-unstable-nested-components */
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components/native';
+import { SafeAreaView, Text } from 'react-native';
 import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
-import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
+import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 import { theme } from './src/infrastructure/theme';
+
+function SettingsScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Settings will be in here</Text>
+    </SafeAreaView>
+  );
+}
+
+function MapScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Map will be in here</Text>
+    </SafeAreaView>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -22,7 +45,32 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                if (route.name === 'Restaurants') {
+                  iconName = focused ? 'fast-food' : 'fast-food-outline';
+                } else if (route.name === 'Settings') {
+                  iconName = focused ? 'settings' : 'settings-outline';
+                } else if (route.name === 'Map') {
+                  iconName = focused ? 'map' : 'map-outline';
+                }
+
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: 'tomato',
+              tabBarInactiveTintColor: 'gray',
+            })}
+          >
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <StatusBar style="auto" />
     </>
